f5320a735c187a7757535f714b36e72139eadc7a
Cabber <xiejunping@caohua.com>
2021-05-14 13:47:49 +0800

    danmo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <vue-danmaku :danmus="danmus" :style="{height: '300px'}"></vue-danmaku>
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App" @on-reg="getReg" />
     <a-button @click="getDelay">手动触发delay</a-button>
@@ -13,18 +14,21 @@
       <vxe-table-column field="sex" title="Sex"></vxe-table-column>
       <vxe-table-column field="age" title="Age"></vxe-table-column>
     </vxe-table>
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import vueDanmaku from 'vue-danmaku'
 import HelloWorld from '@/components/HelloWorld.vue'
 import { userLogin, userRegister, delay } from '@/api/user.model'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    HelloWorld,
+    vueDanmaku
   },
   data () {
     return {
@@ -34,7 +38,8 @@ export default {
         { id: 10002, name: 'Test2', role: 'Test', sex: 'Women', age: 22, address: 'Guangzhou' },
         { id: 10003, name: 'Test3', role: 'PM', sex: 'Man', age: 32, address: 'Shanghai' },
         { id: 10004, name: 'Test4', role: 'Designer', sex: 'Women ', age: 24, address: 'Shanghai' }
-      ]
+      ],
+      danmus: ['danmu1', 'danmu2']
     }
   },
   created () {
@@ -88,3 +93,6 @@ export default {
   }
 }
 </script>
+<style scoped>
+
+</style>
